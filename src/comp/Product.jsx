a7dad6eef2c41285ac04d8d2c40ad70827acd57a
zkpahore <zahirpahore@yahoo.com>
2024-10-20 10:58:02 +0500

    third commit

--- src/comp/Product.jsx
+++ src/comp/Product.jsx
@@ -20,9 +20,7 @@ const Product = ({ products }) => {
       <div className="flex justify-center mb-4 flex-wrap px-4">
         {/* "All" button */}
         <button
-          className={`px-4 py-2 mx-2 rounded m-1 ${
-            selectedCategory === null ? 'bg-blue-500 text-white' : 'bg-gray-300 text-gray-700'
-          }`}
+          className={`px-4 py-2 mx-2 rounded m-1 ${selectedCategory === null ? 'bg-blue-500 text-white' : 'bg-gray-300 text-gray-700'}`}
           onClick={() => setSelectedCategory(null)}
         >
           All
@@ -32,9 +30,7 @@ const Product = ({ products }) => {
         {categories.map(category => (
           <button
             key={category}
-            className={`px-4 py-2 mx-2 rounded m-1 ${
-              selectedCategory === category ? 'bg-blue-500 text-white' : 'bg-gray-300 text-gray-700'
-            }`}
+            className={`px-4 py-2 mx-2 rounded m-1 ${selectedCategory === category ? 'bg-blue-500 text-white' : 'bg-gray-300 text-gray-700'}`}
             onClick={() => handleCategoryClick(category)}
           >
             {category}
@@ -51,7 +47,7 @@ const Product = ({ products }) => {
               <div className="w-72 h-auto bg-white rounded-xl shadow-md p-6 m-4 flex flex-col items-center transform transition-all duration-500 hover:scale-105 hover:shadow-2xl hover:bg-gradient-to-r from-purple-400 to-blue-400 hover:text-white hover:border-transparent border border-gray-200">
                 <img src={product.img} alt={product.name} className="w-52 h-64 mb-4 rounded-lg transform transition-transform duration-500 hover:rotate-3 hover:scale-110" />
                 <div className="font-bold text-lg mb-2 transition-colors duration-500">{product.name}</div>
-                <div className="text-gray-600 transition-colors duration-500">Year: {product.year}</div>
+                <div className="text-gray-600 transition-colors duration-500">Release Year: {product.year}</div>
               </div>
             </Link>
           ))}
